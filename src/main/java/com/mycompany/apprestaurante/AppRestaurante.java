6d4689802ad617f2d5c5172795cf4b1367d5793a
Jonathan <Jonathan@Uri>
2025-06-25 19:00:09 -0500

    Primer commit.

---
+++ src/main/java/com/mycompany/apprestaurante/AppRestaurante.java
@@ -0,0 +1,12 @@
+package com.mycompany.apprestaurante;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class AppRestaurante {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
